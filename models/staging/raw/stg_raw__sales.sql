447a4133c7b307ebe596ed03401facc011334932
HM <email@hubertusmai.com>
2026-02-11 13:01:10 +0000

    Add staging models, documentation and tests

---
+++ models/staging/raw/stg_raw__sales.sql
@@ -0,0 +1,22 @@
+with
+
+source as (
+
+    select * from {{ source('raw', 'sales') }}
+
+),
+
+renamed as (
+
+    select
+        date_date,
+        orders_id,
+        pdt_id as products_id,
+        revenue,
+        quantity as qty
+
+    from source
+
+)
+
+select * from renamed
